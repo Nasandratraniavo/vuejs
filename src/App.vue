1fcdfbfc76203cf64b6741f8e4f82861a86f4fd9
nasandratriniavo <nasandratraniavo06@gmail.com>
2023-12-14 11:28:07 +0300

    vue2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{ saluE }} {{ Nom }}</div>
+  <div v-text="Loisir"></div>
+  <div v-html="Commentaire"></div>
+  <div v-html="Commentaire2"></div>
+  <h2 v-bind:id="HeadingId">Mahafinaritra</h2>
+  <Button v-bind:disabled="isDisabled">Press</Button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      saluE: 'Bonjour',
+      Nom: 'Nasandratra',
+      Loisir: 'Batterie',
+      Commentaire: '<b>Tsara ilay mitendry</b>',
+      Commentaire2: `<a href="#" onclick="alert('Ho Azy irery ihany ny voninahitra')">Tsindrio ne!</a>`,
+      HeadingId: 'footer',
+      isDisabled: false
+    }
+  },
 }
 </script>
 
