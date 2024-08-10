119945a67e643e214964c231515c5d7aabd7fe7d
Nasandratraniavo <nasandratraniavo06@gmail.com>
2024-08-10 07:11:51 +0300

    premiercommit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>{{ saluE }} {{ Nom }}</div>
-  <div v-text="Loisir"></div>
-  <div v-html="Commentaire"></div>
-  <div v-html="Commentaire2"></div>
-  <h2 v-bind:id="HeadingId">Mahafinaritra</h2>
-  <Button v-bind:disabled="isDisabled">Press</Button>
+  <header>
+    <h1>{{ saluE }} {{ Nom }}</h1>
+    <p v-text="Loisir"></p>
+  </header>
+
 </template>
 
 <script>
@@ -13,25 +12,53 @@ export default {
   name: 'App',
   data() {
     return {
-      saluE: 'Bonjour',
-      Nom: 'Nasandratra',
-      Loisir: 'Batterie',
-      Commentaire: '<b>Tsara ilay mitendry</b>',
-      Commentaire2: `<a href="#" onclick="alert('Ho Azy irery ihany ny voninahitra')">Tsindrio ne!</a>`,
-      HeadingId: 'footer',
-      isDisabled: false
+      saluE: 'Bienvenue',
+      Nom: 'Mr Gaetan',
+      Loisir: 'Exercice d\'application Docker, creation container et image',
     }
   },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+body{
+  font-family: 'Open sans', sans-serif;
+  background: #fff;
+}
+header{
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+}
+header::before{
+  content: '';
+  background: url('./assets/trano.jpg') no-repeat center center/cover;
+  top: 0;
+  left: 0;
+  position: absolute;
+  opacity: 0.9;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+header > h1{
+  font-size: 4rem;
+  color: rgb(4, 135, 187);
+  padding: 1rem 0;
+  text-shadow: 1px 1px #fff;
+}
+header > p{
+  font-weight: 600;
+  font-size: 1.8rem;
 }
 </style>
